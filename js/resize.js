553ec748989d02907ad7ca7b5a25ba3542700a0f
pinusc <giuseppe.stelluto@gmail.com>
2016-10-25 19:00:29 +0800

    Remove useless log

--- js/resize.js
+++ js/resize.js
@@ -1,24 +1,24 @@
 function autoSizeText() {
   var el, elements, _i, _len, _results;
   elements = document.querySelectorAll('.resize');
-  console.log(elements);
+  // console.log(elements);
   if (elements.length < 0) {
     return;
   }
   _results = [];
   for (_i = 0, _len = elements.length; _i < _len; _i++) {
     el = elements[_i];
-    console.log(el.scrollHeight, el.offsetHeight);
+    // console.log(el.scrollHeight, el.offsetHeight);
     _results.push((function(el) {
       el.style.fontSize = "300px";
       var resizeText, _results1;
       resizeText = function() {
-        console.log(el.style)
+        // console.log(el.style)
         var elNewFontSize;
         // elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
-        console.log(el.style.fontSize);
+        // console.log(el.style.fontSize);
         elNewFontSize = (parseInt(el.style.fontSize.slice(0, -2)) - 1) + 'px';
-        console.log(elNewFontSize);
+        // console.log(elNewFontSize);
         el.style.fontSize = elNewFontSize;
         // return $(el).css('font-size', elNewFontSize);
         return 1;
